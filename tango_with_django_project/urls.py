e8992fa5c948c32618c0c8918506caf9a28d0b5e
Kasparas Baniulis (student) <2263047B@student.gla.ac.uk>
2020-02-21 14:34:48 +0000

    Initial state

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,4 +1,4 @@
-"""woolley_mammoth URL Configuration
+"""tango_with_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'rango'
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')), 
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
